8632383e1f1e96ef86d1be86612aa39d6b326a4f
Negrea-Andrei <negrea97@yahoo.com>
2023-10-09 19:46:05 +0300

    Start the Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div>
+        <h1>CV App</h1>
+      </div>
+    </header>
+  );
+}
