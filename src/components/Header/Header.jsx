ac74e85c1947236451a322e9e742d1dc6c5e1261
Negrea-Andrei <negrea97@yahoo.com>
2023-10-10 20:54:23 +0300

    Add FontAwesopme for the icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,11 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <header className="header">
-      <h1>Create the perfect CV</h1>      
-      <div className="header-buttons">A</div>
-    </header>
+    <>
+      <header className="header">
+        <h1>Create the perfect CV <i class="fa-regular fa-file-lines fa-xs"></i></h1>     
+        <div className="header-buttons">A</div>
+      </header>
+    </>
   );
 }
