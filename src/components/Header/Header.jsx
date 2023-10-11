c2f4cd4c4fc55440e276f9cdedc3bfd652d72687
Negrea-Andrei <negrea97@yahoo.com>
2023-10-11 22:21:43 +0300

    Add interactivity to the header to change the buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,15 +5,25 @@ export default function Header({ toggleState, viewButton }) {
   return (
     <>
       <header className="header">
-        <h1>Create the perfect CV &nbsp;<i className="fa-regular fa-file-lines fa-xs"></i></h1>
+        <h1>
+          Create the perfect CV &nbsp;
+          <i className="fa-regular fa-file-lines fa-xs"></i>
+        </h1>
         <div className="header-buttons">
-          {viewButton ?
-            <i onClick={toggleState} class="fa-solid fa-eye fa-2xl"></i> :
-            <div className='test'>
-              <i onClick={toggleState} class="fa-solid fa-print fa-2xl"></i>
-              <i onClick={toggleState} class="fa-solid fa-pen fa-2xl"></i>
+          {viewButton ? (
+            <span>
+              <i onClick={toggleState} class="fa-solid fa-eye fa-beat fa-2xl"></i>
+            </span>
+          ) : (
+            <div className="test">
+              <span>
+                <i onClick={toggleState} class="fa-solid fa-print fa-2xl"></i>
+              </span>
+              <span>
+                <i onClick={toggleState} class="fa-solid fa-pen fa-2xl"></i>
+              </span>
             </div>
-          }
+          )}
         </div>
       </header>
     </>
