547bca5507ec73c434af98e25b1f455159c7e6ac
Negrea-Andrei <negrea97@yahoo.com>
2023-10-11 20:10:00 +0300

    Add interactivity to the header to change the buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,19 @@
 import "./Header.css";
+import { useState } from "react";
 
 export default function Header() {
+  const [viewButton, setViewButton] = useState('true')
+
+  const Click = () => {
+    setViewButton(!viewButton)
+  }
   return (
     <>
       <header className="header">
-        <h1>Create the perfect CV <i class="fa-regular fa-file-lines fa-xs"></i></h1>     
-        <div className="header-buttons">A</div>
+        <h1 onClick={Click}>Create the perfect CV <i class="fa-regular fa-file-lines fa-xs"></i></h1>     
+        <div className="header-buttons">
+          {viewButton ?
+        <i onClick={Click} class="fa-regular fa-file-lines fa-2xl"></i> : <i onClick={Click} class="fa-solid fa-car fa-2xl"></i>}</div> 
       </header>
     </>
   );
