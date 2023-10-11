05cdc63555ea2834ecbdea91bcb07a4665acf38f
Negrea-Andrei <negrea97@yahoo.com>
2023-10-11 21:45:25 +0300

    Add interactivity to the header to change the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 
 function App() {
+  const [viewButton, setViewButton] = useState(true);
+
+  const Click = () => {
+    setViewButton(!viewButton);
+  }
+
   return (
     <div className="container">
-      <Header />
+      <Header Click={Click} viewButton={viewButton} />
+      {viewButton ? <div>A</div> : <div>B</div>}
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,20 @@
+import React from "react";
 import "./Header.css";
-import { useState } from "react";
 
-export default function Header() {
-  const [viewButton, setViewButton] = useState('true')
-
-  const Click = () => {
-    setViewButton(!viewButton)
-  }
+export default function Header({ Click, viewButton }) {
   return (
     <>
       <header className="header">
-        <h1 onClick={Click}>Create the perfect CV <i class="fa-regular fa-file-lines fa-xs"></i></h1>     
+        <h1 onClick={Click}>Create the perfect CV <i className="fa-regular fa-file-lines fa-xs"></i></h1>
         <div className="header-buttons">
           {viewButton ?
-        <i onClick={Click} class="fa-regular fa-file-lines fa-2xl"></i> : <i onClick={Click} class="fa-solid fa-car fa-2xl"></i>}</div> 
+            <i onClick={Click} class="fa-solid fa-eye fa-2xl"></i> :
+            <div className='test'>
+              <i onClick={Click} class="fa-solid fa-print fa-2xl"></i>
+              <i onClick={Click} class="fa-solid fa-pen fa-2xl"></i>
+            </div>
+          }
+        </div>
       </header>
     </>
   );
