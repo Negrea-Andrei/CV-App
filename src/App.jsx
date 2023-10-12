138bf73bf9c4a7da4d601be8b5819fe3df5a5aad
Negrea-Andrei <negrea97@yahoo.com>
2023-10-12 19:17:58 +0300

    Reset the components. Need to stile them individualy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Description from "./components/Description/Description";
 import Info from "./components/Info/Info";
-import Education from "./components/Education/Education";
 
 function App() {
   const [viewButton, setViewButton] = useState(true);
@@ -15,28 +13,14 @@ function App() {
   return (
     <div className="container">
       <Header toggleState={toggleState} viewButton={viewButton} />
-      <div className="sections-container">
         {viewButton ? (
           <div className="contain">
-            <section>
-              <Description />
-            </section>
-            <section>
-              <Info />
-            </section>
-            <section>
-              <span>Education</span>
-              <Education />
-            </section>
-            <section>              
-              <span>Work Experience</span>
-            </section>
+            <Info />           
           </div>
         ) : (
           <div>NOT YET</div>
         )}
       </div>
-    </div>
   );
 }
 
