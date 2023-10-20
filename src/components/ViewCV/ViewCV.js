b2a52aef5650c768e4aae6c745623807593e3927
Negrea-Andrei <negrea97@yahoo.com>
2023-10-20 21:27:18 +0300

    Add the function to the ViewCV component

--- src/components/ViewCV/ViewCV.js
+++ src/components/ViewCV/ViewCV.js
@@ -1,1 +1,24 @@
 import 'ViewCV.css'
+
+export default function ViewCV({
+    info,
+    description,
+    education,
+    experience,
+  }) {
+    return (
+      <div className="cv-preview-container">
+        <CvHeader
+          name={generalInfo.name}
+          phone={generalInfo.phone}
+          address={generalInfo.address}
+          email={generalInfo.email}
+        />
+        <CvDescription description={description} />
+        <CvEducation education={education} />
+        <CvExperience experience={experience} />
+      </div>
+    );
+  }
+  
+  ;
