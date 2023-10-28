8733841c45b9fec0ebd1dcb088ff651f22601f72
Negrea-Andrei <negrea97@yahoo.com>
2023-10-28 18:59:01 +0300

    Delete the download feature. The print feature includes it

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,7 @@
-import React, { useEffect } from "react";
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
 import "./Header.css";
 
-const downloadCV = () => {
-  const divToDownload = document.querySelector(".view-container");
-
-  if (divToDownload) {
-    html2canvas(divToDownload, { useCORS: true }).then(function (canvas) {
-      const imgData = canvas.toDataURL("image/png");
-
-      const pdf = new jsPDF("p", "mm", "a4");
-
-      const imgHeight = 297;
-      const imgWidth = 210;
-
-      pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
-
-      pdf.save("cv.pdf");
-    });
-  }
-};
 
 export default function Header({ toggleState, viewButton, Print }) {
-  useEffect(() => {}, []);
-
   return (
     <>
       <header className="header no-print">
@@ -38,18 +15,10 @@ export default function Header({ toggleState, viewButton, Print }) {
               <i onClick={toggleState} className="fa-solid fa-eye fa-2xl"></i>
             </span>
           ) : (
-            <div className="actions">
-              <div className="cv-actions">
+            <div className="actions">              
                 <span>
                   <i onClick={Print} className="fa-solid fa-print fa-2xl"></i>
                 </span>
-                <span>
-                  <i
-                    onClick={downloadCV}
-                    className="fa-solid fa-download fa-2xl"
-                  ></i>
-                </span>
-              </div>
               <span>
                 <i onClick={toggleState} className="fa-solid fa-pen fa-2xl"></i>
               </span>
